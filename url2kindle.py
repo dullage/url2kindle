ad3b6005911c0415b582c51ae18b5d5aee805d9b
Adam Dullage <adam@dullage.com>
2022-01-27 21:30:42 +0000

    Fixing encoding issues

--- url2kindle.py
+++ url2kindle.py
@@ -8,6 +8,19 @@ import magic
 import requests
 from bs4 import BeautifulSoup
 
+HTML_TEMPLATE = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+</head>
+<body></body>
+</html>
+"""
+
 PARSER_PATH = os.environ.get("PARSER_PATH", "mercury-parser")
 KINDLE_APPRISE_URL = os.environ["KINDLE_APPRISE_URL"]
 KINDLE_TAG = "kindle"
@@ -34,7 +47,7 @@ def embed_image(src: str) -> str:
     mime = magic.from_buffer(image, mime=True)
     if not mime.startswith("image/"):
         return src
-    image_base64 = b64encode(image).decode("utf8")
+    image_base64 = b64encode(image).decode("utf-8")
     return f"data:{mime};base64,{image_base64}"
 
 
@@ -47,16 +60,17 @@ def embed_images(soup: BeautifulSoup) -> BeautifulSoup:
 
 def url2kindle(url):
     try:
-        mercury_output_json = check_output([PARSER_PATH, url])
+        mercury_output_json = check_output([PARSER_PATH, url]).decode()
         mercury_output = json.loads(mercury_output_json)
         body_content = BeautifulSoup(mercury_output["content"], "html.parser")
         body_content = embed_images(body_content)
 
-        html = BeautifulSoup("<html><body></body></html>", "html.parser")
+        html = BeautifulSoup(HTML_TEMPLATE, "html.parser")
         html.body.append(body_content)
+        html.head.title.append(mercury_output["title"])
 
         filename = f"{safe_filename(mercury_output['title'])}.html"
-        with open(filename, "w", encoding="utf8") as f:
+        with open(filename, "w", encoding="utf-8-sig") as f:
             f.write(str(html))
         sender.notify(
             title=f"url2kindle",
